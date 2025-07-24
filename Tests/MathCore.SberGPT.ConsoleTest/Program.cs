0f79e9190967eeff92e96bf62637c45aa5a95063
Infarh <shmachilin@gmail.com>
2025-07-24 21:45:46 +0300

    Убираем хлам

--- Tests/MathCore.SberGPT.ConsoleTest/Program.cs
+++ Tests/MathCore.SberGPT.ConsoleTest/Program.cs
@@ -4,52 +4,7 @@ using MathCore.SberGPT.Attributes;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-const string token = "eyJjdHk-------3QiLCJlbmMiOiJBM--------------iYWxnIjoiUlNBLU9B---------In0.uCM4bgxJO-------------Fn8sqpx3d52qJ_SecYxdCuROwk_q4EV-----1hp4ECTUV_cTEjBYmtTxVTl6cpcKjC8-z-ukWOa-------sxLQ-Cb39_iApVpOvjsjaxnwR_GLG2fAGBcbn57kgUCdKMnvNmvLEOH0s25pH0Bd3jhdA5DtU6GUM1TgDXLXFokXlQJPSX4lxpD50OtMpjk-umwV3M7Hlm4Cmwgts9RAtqmK_zdSOsFOgmk9NgFAlRnR14nlqB3eBW7g62JVzcmLCuzy_HPy7CxDIwQWcib5vynrO1RwuMwue91o5QpZn7Q.ibb1LIdLko4Yfn5uQJv6Xg.ORM-2zGX6SNKMEDswOrjskzMEe2vbQR90PdfOzciyhGRSsYChZxDSJZe67tE1gsP2bn5DqgsuDhdjppXgQ6R-lz26iUl8_LG8IZRLsW_s7FZlQEoC15jeu2j9WwoS_-v8sSZFMCKwPQCKbOnB31qsnmIcbtYRQQr15y8mMZaRoO616-BfdSnpiGdDDjpUJkgFTmn0uVavxjcKsL6hYGYNxZ_rQZoXO0T09TRMNoNYb07lrcIidEFjLiNiP0RH7SwdUUQa2x0fd6mMl09H7BWwTS5h3c9H0lWn_vkliwPwtzBJ6mYZ7iv8Df1TXDoPsyoL-rahputZUSezqorHg-vEERn8VXdtKuAazG6qUFXP8QGx6wjCWG2xgH9Wbh64F3flZZQWOS1bya2xEu1K8-x3GMyoDKxHHEIAL4X71qcFT1Je179Vzc9WT7YRKDFqwTMzWTQdfVjLGRNSUp8WLcTePY78TIQA5ccmtuLOMWchpHHeydq4vsJ5LaY4zfSZp7Gqj7XzsFDf9MTttx-yvGHNmopW0ofFGUfJht8PVwfBg9TWENqIfCXwFKglDVPvOH85nO92i0V-mNiM2q9MCbOXAQQKYTLSmhIUCVo-AweinOeQj_G9ufsAC0et4MHno4HUNVhrCYRZajQWKONlrV9DvqfBSylXdFa0R00mbaT1ZcnznxSQMwzGIzuBBCSon_taxFvgQwphEu8P5c_unzVoi4Xwt6v0xfHtsfW4OhurUY.ADO0gk--------JAvpkexSy9t6yTtUw";
-const string data_path = "d:\\TestFile.txt";
 
-using (var client = new HttpClient())
-{
-    var request = new HttpRequestMessage(HttpMethod.Post, "https://gigachat.devices.sberbank.ru/api/v1/files");
-
-    request.Headers.Accept.ParseAdd("*/*");
-    request.Headers.Authorization = new("Bearer", token);
-    request.Headers.UserAgent.ParseAdd("MathCore.SberGPT/1.0");
-
-    var content = new MultipartFormDataContent
-    {
-    };
-    content.Add(new StreamContent(File.OpenRead(data_path)), "file", "file.txt");
-    request.Content = content;
-
-    try
-    {
-        using var response = await client.SendAsync(request);
-        var response_body = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(response_body);
-    }
-    catch (HttpRequestException e)
-    {
-        Console.WriteLine(e.Message);
-    }
-}
-
-if (false)
-    using (var http = new HttpClient())
-    {
-        http.DefaultRequestHeaders.Authorization = new("Bearer", token);
-        http.DefaultRequestHeaders.UserAgent.ParseAdd("MathCore.SberGPT/1.0");
-
-        using var form = new MultipartFormDataContent
-    {
-        { new StreamContent(File.OpenRead(data_path)), "file", "TestFile.txt" },
-        { new StringContent("general"), "purpose" }
-    };
-
-        var response = await http.PostAsync("https://gigachat.devices.sberbank.ru/api/v1/", form);
-
-        var str = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(str);
-    }
 
 var cfg = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", true)
